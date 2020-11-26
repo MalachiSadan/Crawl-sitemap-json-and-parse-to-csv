cb712df9f33dd9abece933dd7650c3389c91af8d
Malachi Sadan <49761570+malmal12@users.noreply.github.com>
2020-11-26 15:13:47 +0200

    Update crawl_sitemap.py

--- crawl_sitemap.py
+++ crawl_sitemap.py
@@ -6,12 +6,12 @@ with open("roku.sitemap.txt", "r") as sitemap_txt:  # the url file may not be up
     while True:
         readlin = sitemap_txt.readline()    # get the app marketplace url
         cnt_da_lp += 1
-        print(cnt_da_lp)  # print every time the loop runs. you can know wht line has a problem.
+        print(cnt_da_lp)  # print every time the loop runs. you can know what line has a problem.
         if readlin == "":
-            break
+            break  # end the loop when reaches end of file
 
         with open(f"ROKU_SITEMAP_JSON/{readlin[59:-24]}.json", "x") as sitemap:
-            # ^ create a json file with app id from the url ^
+            # ^ create a JSON file with app id from the url(change the index for getting app id's in other websites) ^
             reqst = http.request("GET", readlin, preload_content=False)  # GET request the url
             data = str(reqst.read())[2:-1]  # read the data without apostrophes
             sitemap.write(data)  # load the data into the file
